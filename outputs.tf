df0b98fbb95e7b553ea1612a5ec1c255b367ad3e
Uziel Sulkies <uziel@next-insurance.com>
2021-12-13 17:51:56 +0200

    Add terraform module code, equivalent to cloudformation

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "monitoring_lambda" {
+  description = "The aws_lambda_function output"
+  vaule       = aws_lambda_function.monitoring_lambda
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "lambda_function_name" {
+  type        = string
+  default     = "VpcSubnetIpMonitorFunction"
+  description = "The function name for the created lambda"
+}
+
+variable "lambda_invocation_rate_minutes" {
+  type        = number
+  default     = 5
+  description = "Fire the lambda every X minutes"
+}
+
+variable "lambda_execution_policy_name" {
+  type        = string
+  default     = "VpcSubnetIpMonitorExecutionPolicy"
+  description = "The IAM policy name for the lambda execution permissions"
+}
+
+variable "lambda_execution_role_name" {
+  type        = string
+  default     = "VpcSubnetIpMonitorRole"
+  description = "The IAM role name for the lambda execution permissions"
+}
